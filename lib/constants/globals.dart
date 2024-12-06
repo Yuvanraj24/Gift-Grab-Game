0a0da81e165498c5e9efcd09e114dbfe5ab7002e
Yuvanraj <itsmeyuvanraj@gmail.com>
2024-12-06 17:31:54 +0530

    some updated

--- lib/constants/globals.dart
+++ lib/constants/globals.dart
@@ -5,11 +5,14 @@ class Globals {
   static const String santaFrozen = 'santa-frozen.png';
   static const String santaSlideLeftSprite = 'santa-slide-left.png';
   static const String santaSlideRightSprite = 'santa-slide-right.png';
+
   static const String backgroundSprite = 'background-sprite.jpg';
   static const String giftSprite = 'gift-sprite.png';
   static const String iceSprite = 'ice-sprite.png';
   static const String flameSprite = 'flame.png';
   static const String cookieSprite = 'cookie.png';
 
+
+  static const String itemGrabSound = 'item-grab-sound.wav';
 
 }
--- lib/games/gift_grab_game.dart
+++ lib/games/gift_grab_game.dart
@@ -1,18 +1,37 @@
+import 'package:flame/collisions.dart';
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:gift_open/components/background_component.dart';
+import 'package:gift_open/components/gift_component.dart';
+import 'package:gift_open/components/ice_component.dart';
 import 'package:gift_open/components/santa_component.dart';
+import 'package:gift_open/constants/globals.dart';
 import 'package:gift_open/inputs/joystick.dart';
+import 'package:flame_audio/flame_audio.dart';
 
-class GiftGrabGame extends FlameGame {
+class GiftGrabGame extends FlameGame with DragCallbacks, HasCollisionDetection {
+  int score = 0;
+
   @override
   Future<void> onLoad() async {
     await super.onLoad();
 
     add(BackgroundComponent());
 
-    add(SantaComponent());
+    add(SantaComponent(joystick: joystick));
+
+    add(GiftComponent());
 
     add(joystick);
+
+    FlameAudio.audioCache.loadAll([
+      Globals.itemGrabSound,
+    ]);
+    
+    
+    add(IceComponent(startPosition: Vector2(200, 200)));
+    add(IceComponent(startPosition: Vector2(size.x - 200, size.y - 200)));
+    
+    add(ScreenHitbox());
   }
 }
